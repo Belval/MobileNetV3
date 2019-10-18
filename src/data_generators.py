96ac4598a4d019781285e424f215acdf235a5b5c
Edouard Belval <edouard@belval.org>
2019-10-17 20:28:50 -0400

    Add troubleshooting for data_generator

--- src/data_generators.py
+++ src/data_generators.py
@@ -19,15 +19,16 @@ def coco_data_generator(path, batch_size, class_count):
 
         while True:
             images = np.zeros((batch_size, 448, 448, 3), dtype=np.uint8)
-            labels = np.zeros((batch_size, 448 // 8, 448 // 8, class_count), dtype=np.uint8)
+            labels = np.zeros((batch_size, class_count, 448 // 8, 448 // 8), dtype=np.uint8)
             count = 0
             for image_id, segmentation in coco.imgToAnns.items():
                 image = Image.open(files[image_id])
                 images[count, :, :, :] = resize_and_crop(image, 448)
-
-                for ann in segmentation:
-                    image = Image.fromarray(coco.annToMask(ann))
-                    labels[count, :, :, ann["category_id"] - 1] += resize_and_crop(image, 448 // 8, rgb=False)
+                for i, ann in enumerate(segmentation):
+                    arr = coco.annToMask(ann)
+                    image = Image.fromarray(arr)
+                    resized = resize_and_crop(image, 448 // 8, rgb=False)
+                    labels[count, ann["category_id"] - 1, :, :] += resized
                 labels[count, labels[count, :, :, :] > 0] = 1
                 count += 1
                 if count == batch_size:
@@ -37,3 +38,13 @@ def coco_data_generator(path, batch_size, class_count):
                     count = 0
 
     return generator(coco, path, batch_size, class_count), len(coco.imgToAnns)
+
+if __name__ == '__main__':
+    train_generator, c1 = coco_data_generator(
+        "data/coco/train/instances_train2017.json",
+        batch_size=32,
+        class_count=100,
+    )
+
+    for blah, bleh in train_generator:
+        print('Blah')
